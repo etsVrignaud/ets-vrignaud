81ee5773b374541a12fa6544611252615059a28a
Benoit Vrignaud <benoa.ets_vrignaud@yahoo.com>
2025-07-25 11:49:45 +0200

    ajout landing pages services menuiserie

---
+++ qr-sitetest.py
@@ -0,0 +1,12 @@
+﻿import qrcode
+
+# Le contenu du QR code (texte ou URL)
+data = "https://etsvrignaud.github.io/ets-vrignaud/"
+
+# Génération du QR code
+qr = qrcode.make(data)
+
+# Enregistrement dans un fichier image
+qr.save("mon_qrcode.png")
+
+print("QR code généré avec succès !")
